7aecccc4af1c13f7406253d2a4b24004822638b1
Hossein <hhasnabadi@yahoo.com>
2025-03-09 14:10:05 +0330

    Header

--- aoutoRent/src/components/Header/Header.jsx
+++ aoutoRent/src/components/Header/Header.jsx
@@ -1,12 +1,36 @@
 import React from "react";
 import Navbar from "../Navnar/Navbar";
+import { Link } from "react-router";
 
 function Header() {
   return (
     <header
-      className='min-h-screen relative bg-[url("/headerbg.png") overflow-hidden bg-center bg-cover flex flex-col   '
+      className="min-h-screen relative overflow-hidden bg-center bg-cover flex flex-col"
       style={{ backgroundImage: "url('/headerbg.png')" }}
     >
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
+        <h1 className="text-amber-300 text-3xl sm:text-4xl md:text-5xl leading-relaxed">
+          اُتـــو رِنت؛ سریع، آسان و به صرفه
+        </h1>
+        <span className="text-white text-lg sm:text-xl md:text-2xl">
+          سرویس دهنده رزرو خودرو در ایران در کمترین زمان ممکن!
+        </span>
+        <div className="flex justify-center mt-6 gap-4 px-6 md:px-9">
+          <button className="text-black flex items-center border bg-amber-300 rounded px-6 py-2">
+            <Link to={"/"} className="flex justify-center gap-2">
+              <img src="/car.png" alt="Car" className="w-6 h-6" />
+              رزرو آسان خودرو
+            </Link>
+          </button>
+          <button className="text-white flex items-center px-5 py-2 border bg-transparent rounded">
+            <Link to={"/"} className="flex justify-center gap-2">
+              <img src="/car.png" className="w-6 h-6" alt="Contact" />
+              تماس با ما
+            </Link>
+          </button>
+        </div>
+      </div>
+
       <Navbar />
     </header>
   );
--- aoutoRent/src/components/Navnar/Navbar.jsx
+++ aoutoRent/src/components/Navnar/Navbar.jsx
@@ -1,16 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router";
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false); // کنترل باز و بسته شدن منو
+
   return (
-    <nav className="container  bg-white absolute  w-1/2 z-20 top-0 start-0 flex left-1/2 -translate-x-1/2 rounded ">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <Link
-          to={"/"}
-          className="flex items-center space-x-3 rtl:space-x-reverse"
-        >
-          <img src="/Logo.png" className="h-8" alt=" Logo" />
-          {/* <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"></span> */}
-        </Link>
+    <nav className="container  bg-white absolute w-3/4 top-0  flex left-1/2 -translate-x-1/2   rounded ">
+      <div className="max-w-screen-xl w-full flex flex-wrap items-center justify-between mx-auto p-4">
+        {/* دکمه همبرگر و لوگو کنار هم با فاصله */}
+        <div className="flex items-center space-x-4 rtl:space-x-reverse md:space-x-4">
+          {/* دکمه منو برای موبایل با فاصله */}
+          <button
+            onClick={() => setIsOpen(!isOpen)}
+            className="md:hidden text-gray-500 focus:outline-none"
+          >
+            <svg
+              className="w-6 h-6 cursor-pointer"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 6h16M4 12h16m-7 6h7"
+              />
+            </svg>
+          </button>
+
+          <Link
+            to={"/"}
+            className="flex items-center space-x-3 rtl:space-x-reverse"
+          >
+            <img src="/Logo.png" className="h-8" alt=" Logo" />
+            {/* <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"></span> */}
+          </Link>
+        </div>
         <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
           <button
             type="button"
@@ -20,18 +46,15 @@ function Navbar() {
           </button>
         </div>
         <div
-          className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
-          id="navbar-sticky"
+          className={`w-full md:flex md:w-auto md:order-1 ${isOpen ? "block" : "hidden"}`}
         >
           <ul
-            className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg
-           bg-gray-50 md:space-x-8 md:flex-row md:mt-0 md:border-0 md:bg-white 
-             dark:border-gray-700"
+            className="flex flex-col p-4 md:p-0 mt-4 font-medium  rounded-lg bg-gray-50 md:space-x-8 md:flex-row md:mt-0 md:border-0 md:bg-white"
           >
             <li>
               <a
                 href="#"
-                className="block py-2 px-3 text-white  rounded-sm md:bg-transparent md:p-0"
+                className="block py-2 px-3 text-gray-400 rounded-sm md:bg-transparent md:p-0"
                 aria-current="page"
               >
                 رزرو خودرو
@@ -40,7 +63,7 @@ function Navbar() {
             <li>
               <a
                 href="#"
-                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent"
               >
                 خدمات ما
               </a>
@@ -48,7 +71,7 @@ function Navbar() {
             <li>
               <a
                 href="#"
-                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent"
               >
                 بلاگ
               </a>
@@ -56,12 +79,12 @@ function Navbar() {
             <li>
               <a
                 href="#"
-                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent"
               >
                 تماس با ما
               </a>
             </li>
-            <img src="/search.png" alt="" className="w-3" />
+            <img src="/search.png" alt="search" className="w-3" />
           </ul>
         </div>
       </div>
