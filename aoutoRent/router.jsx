624bedb5409c846d65298be97c43314f1adee5d3
Hossein <hhasnabadi@yahoo.com>
2025-03-11 17:47:56 +0330

    login

--- aoutoRent/router.jsx
+++ aoutoRent/router.jsx
@@ -1,9 +1,11 @@
 import Home from "./src/page/Home/Home";
 import Login from "./src/page/Login/Login";
+import Register from "./src/page/Refester/register ";
 
 let routes = [
     { path: "/", element: <Home /> },
-    { path: "/login", element: <Login /> }
+    { path: "/login", element: <Login /> },
+    { path: "/register ", element: <Register /> }
 ];
 
 export default routes;
--- aoutoRent/src/components/Car/Car.jsx
+++ aoutoRent/src/components/Car/Car.jsx
@@ -1,58 +1,71 @@
-import React from 'react'
-import { Link } from 'react-router'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router";
 function Car() {
+  const [car, setCar] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/carData")
+      .then((response) => response.json())
+      .then((data) => setCar(data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
   return (
-    <section className=' container w-full  flex justify-center  md:mt-25  pr-5 '>
-      <div className='flex justify-center items-center flex-wrap gap-4 '>
+    <section className="container w-full  flex justify-center  md:mt-25  pr-5 flex-wrap ">
+      {car.map((item, index) => (
+        <div
+          key={index}
+          className="bg-white border-white shadow-2xl rounded-2xl flex  flex-col text-center "
+        >
+          <Link>
+            <p className="text-center">{item.title}</p>
+            <img
+              src={item.image}
+              className="w-full h-auto max-w-xs mx-auto"
+              alt={item.title}
+            />
+          </Link>
+        </div>
+      ))}
+
+      {/* <div className='bg-white border-white shadow-2xl rounded-2xl '>
+
+<Link>
+  <p className='text-center'>کوپه
+    </p>
+    <img src="/copeh.png" className=' ' alt="" />
+</Link>
+</div>
+<div className='bg-white border-white shadow-2xl rounded-2xl '>
+
+<Link>
+  <p className='text-center'>کوپه
+    </p>
+    <img src="/copeh.png" className=' ' alt="" />
+</Link>
+</div>
+<div className='bg-white border-white shadow-2xl rounded-2xl '>
+
+<Link>
+  <p className='text-center'>کوپه
+    </p>
+    <img src="/copeh.png" className=' ' alt="" />
+</Link>
+</div>
+<div className='bg-white border-white shadow-2xl rounded-2xl '>
+
+<Link>
+  <p className='text-center'>کوپه
+    </p>
+    <img src="/copeh.png" className=' ' alt="" />
+</Link>
+</div>
 
         
-<div className='bg-white border-white shadow-2xl rounded-2xl '>
-
-<Link>
-  <p className='text-center'>کوپه
-    </p>
-    <img src="/copeh.png" className=' ' alt="" />
-</Link>
-</div>
-<div className='bg-white border-white shadow-2xl rounded-2xl '>
-
-<Link>
-  <p className='text-center'>کوپه
-    </p>
-    <img src="/copeh.png" className=' ' alt="" />
-</Link>
-</div>
-<div className='bg-white border-white shadow-2xl rounded-2xl '>
-
-<Link>
-  <p className='text-center'>کوپه
-    </p>
-    <img src="/copeh.png" className=' ' alt="" />
-</Link>
-</div>
-<div className='bg-white border-white shadow-2xl rounded-2xl '>
-
-<Link>
-  <p className='text-center'>کوپه
-    </p>
-    <img src="/copeh.png" className=' ' alt="" />
-</Link>
-</div>
-<div className='bg-white border-white shadow-2xl rounded-2xl '>
-
-<Link>
-  <p className='text-center'>کوپه
-    </p>
-    <img src="/copeh.png" className=' ' alt="" />
-</Link>
-</div>
-        
         
 
-</div>
-
-        </section>
-  )
+</div> */}
+    </section>
+  );
 }
 
-export default Car
+export default Car;
